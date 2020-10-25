6de3c656338773f6a5d19e54b3b663f3f9c92557
Rohan Maheshwari <rohan.maheshwari997@gmail.com>
2020-10-25 10:56:33 +0530

    modularised and added docs

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Install using
 <pre><code>pip install &lt;package_name&gt;</code></pre>
 
 # Usage
-Put the file to check for plagiarism in TEST folder and run as 
-<pre><code>python main.py</code></pre>
+Put the file to check for plagiarism in TEST folder in root directory and run as 
+<pre><code>python main.py TEST/<filename></code></pre>
--- main.py
+++ main.py
@@ -9,6 +9,16 @@ import numpy as np
 #ensure you have Python 3.6+
 
 def extractParas(text):
+	"""
+	Extract paras from the raw text provided
+
+	Parameters:  
+	str: raw text data from the input file  
+
+	Returns:  
+	list: List of paragraphs from the input document  
+	"""
+
 	paragraphs = []
 	para = ""
 	i = 0
@@ -26,6 +36,16 @@ def extractParas(text):
 	return paragraphs
 
 def tokenize(text):
+	"""
+	Tokenize the given paragraph
+
+	Parameters:  
+	list: List of paragraphs from the input document  
+
+	Returns:  
+	list: List of tokens from the input paragraph  
+	"""
+
 	tokenizer = RegexpTokenizer(r'\w+')
 	tokens = tokenizer.tokenize(text)
 
@@ -35,17 +55,138 @@ def tokenize(text):
 	return tokens
 
 def stopwordRemoval(tokens):
-	#Remove stop words
+	"""
+	Remove common English stopwords from the tokens
+	
+	Parameters:  
+	list: List of tokens  
+
+	Returns:  
+	list: List of tokens without the stop words  
+	"""
+
 	stop_words = set(stopwords.words('english'))
 	tokens = [w for w in tokens if not w in stop_words]
 	return tokens
 
 def stemmer(tokens):
-	#Perform stemming
+	"""
+	Stemming function
+
+	Parameters:  
+	list: List of tokens without stopwords  
+
+	Returns:  
+	list: List of stemmed tokens  
+	"""
 	stemmer = PorterStemmer()
 	for i, token in enumerate(tokens):
 		tokens[i] = stemmer.stem(token)
 	return tokens
+
+def createInvertedIndex(documents):
+	"""
+	Creating the inverted index
+
+	Parameters:  
+	documents(list): Preprocessed list containing content of the documents  
+
+	Returns:  
+	Dictionary: The inverted index  
+	&nbsp;&nbsp; Key: word  
+	&nbsp;&nbsp; Value: posting list  
+	"""
+
+	inverted_index = {}
+	for i,doc in enumerate(documents):
+		for j, para in enumerate(doc):
+			for word in para:
+				# print(word)
+				if inverted_index.get(word,False):
+					if i not in inverted_index[word]:
+						inverted_index[word].append(i)
+				else:
+					inverted_index[word] = [i]
+	return inverted_index
+
+
+def calculate_TfIdf_Weights(vocab, inverted_index, documents):
+	"""
+	Calculate the tfidf scores 
+
+	Parameters:  
+	(arg1)vocab(set): Vocabulary of training data  
+	(arg2)inverted_index(dict): Inverted Index  
+	(arg3)documents(list): Preprocessed list containing content of the documents  
+
+
+	Returns:  
+	Dictionary: TfIdf scores  
+	&nbsp;&nbsp; Key: word  
+	&nbsp;&nbsp; Value: list of tf * idf scores for all the documents  
+	"""
+	
+	tf_idf = {}
+	count = 0
+	for word in vocab:
+		for i,doc in enumerate(documents):
+			if i in inverted_index[word]:
+				for para in doc:
+					for w in para:
+						if(w == word):
+							count+=1
+			else:
+				count = 0
+
+			if (i != 0):
+				tf_idf[word].append(count)
+			else:
+				tf_idf[word] = [count]
+			count = 0
+
+	#convert to 1 + log(tf)
+	for word in vocab:
+		tf_idf[word] = [(1 + math.log(x+1)) for x in tf_idf[word]]
+
+	#add idf weighting
+	totaldocs = len(documents)
+	for word in vocab:
+		idfval = math.log(totaldocs/len(inverted_index[word]))
+		tf_idf[word] = [round(x * idfval, 3) for x in tf_idf[word]]
+
+	return tf_idf
+
+
+def rankDocsBySimilarity(documents, testparagraphs, tf_idf):
+	"""
+	Calculate the ranking of the docs wrt similarity to testdoc 
+
+	Parameters:  
+	(arg1)documents(set): Preprocessed list containing content of the documents  
+	(arg2)testparagraphs(list): paragraphs extracted from test document  
+	(arg3)tf_idf(dict): TfIdf scores  
+	&nbsp;&nbsp; Key: word  
+	&nbsp;&nbsp; Value: list of tf * idf scores for all the documents  
+
+	Returns:  
+	Dictionary: Ranking  
+	&nbsp;&nbsp; Key: doc number  
+	&nbsp;&nbsp; Value: Similarity score  
+	"""
+
+	ranking = {} 
+
+	for i,doc in enumerate(documents):
+		ranking[i] = 0
+		for j,para in enumerate(testparagraphs):
+			for word in para:
+				ranking[i] += tf_idf[word][i]
+		ranking[i] = round(ranking[i],3)
+	
+	#sort ranking dict in reverse order by keys
+	ranking = {k: v for k, v in sorted(ranking.items(),reverse=True, key=lambda item: item[1])}
+	return ranking
+
 
 if __name__ == '__main__':
 	documents=[]
@@ -68,23 +209,8 @@ if __name__ == '__main__':
 				for term in finaltokens:
 					vocab.add(term)
 			documents.append(paragraphs)
+	
 	vocablen = len(vocab)
-
-	#creating the inverted index
-	# indexer = Indexer(documents)
-
-	inverted_index = {}
-	for i,doc in enumerate(documents):
-		for j, para in enumerate(doc):
-			for word in para:
-				# print(word)
-				if inverted_index.get(word,False):
-					if i not in inverted_index[word]:
-						inverted_index[word].append(i)
-				else:
-					inverted_index[word] = [i]
-
-	# print(len(inverted_index))
 
 	doclen = [] #length of the docs
 	totcount = 0
@@ -94,37 +220,13 @@ if __name__ == '__main__':
 			totcount += len(para)
 		doclen.append(totcount)
 
-	tf_idf = {}
-	for word in vocab:
-		for i,doc in enumerate(documents):
-			if i in inverted_index[word]:
-				for para in doc:
-					for w in para:
-						if(w == word):
-							count+=1
-				# count/=doclen[i]
-			else:
-				count = 0
 
-			if (i != 0):
-				tf_idf[word].append(count)
-			else:
-				tf_idf[word] = [count]
-			count = 0
-	#tf_idf currently stores only tf values for words in dictionary format
 
-	#convert to 1 + log(tf)
-	for word in vocab:
-		tf_idf[word] = [(1 + math.log(x+1)) for x in tf_idf[word]]
-
-	#add idf weighting
-	totaldocs = len(documents)
-	for word in vocab:
-		idfval = math.log(totaldocs/len(inverted_index[word]))
-		tf_idf[word] = [round(x * idfval, 3) for x in tf_idf[word]]
-
+	inverted_index = createInvertedIndex(documents)
 	
-	# print(tf_idf["inherit"]) #to check whether index is properly created
+
+	tf_idf = calculate_TfIdf_Weights(vocab, inverted_index, documents)
+
 
 
 	#take testfile name from cl arguments
@@ -145,17 +247,10 @@ if __name__ == '__main__':
 		testparagraphs.append(finaltokens)
 
 
-	ranking = {} #dictionary to store doc number and score
 
-	for i,doc in enumerate(documents):
-		ranking[i] = 0
-		for j,para in enumerate(testparagraphs):
-			for word in para:
-				ranking[i] += tf_idf[word][i]
-		ranking[i] = round(ranking[i],3)
+	ranking = rankDocsBySimilarity(documents, testparagraphs, tf_idf)
 	
-	#sort ranking dict in reverse order by keys
-	ranking = {k: v for k, v in sorted(ranking.items(),reverse=True, key=lambda item: item[1])}
+
 
 	#print top 10 matching documents
 	print("\nTop 10 documents matching the given test document in ranked order are: ")
