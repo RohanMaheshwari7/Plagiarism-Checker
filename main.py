fa6b32c772ec4952a41adf53af18118dd1b23f05
Rohan Maheshwari <rohan.maheshwari997@gmail.com>
2020-10-25 18:20:40 +0530

    para as a query

--- main.py
+++ main.py
@@ -157,12 +157,12 @@ def calculate_TfIdf_Weights(vocab, inverted_index, documents):
 	return tf_idf
 
 
-def rankDocsBySimilarity(documents, testparagraphs, tf_idf):
+def rankDocsByMatchingSimilarity(documents, testparagraphs, tf_idf):
 	"""
-	Calculate the ranking of the docs wrt similarity to testdoc 
+	Calculate the ranking of the docs wrt matching similarity to testdoc 
 
 	Parameters:  
-	(arg1)documents(set): Preprocessed list containing content of the documents  
+	(arg1)documents(list): Preprocessed list containing content of the documents  
 	(arg2)testparagraphs(list): paragraphs extracted from test document  
 	(arg3)tf_idf(dict): TfIdf scores  
 	&nbsp;&nbsp; Key: word  
@@ -186,6 +186,131 @@ def rankDocsBySimilarity(documents, testparagraphs, tf_idf):
 	#sort ranking dict in reverse order by keys
 	ranking = {k: v for k, v in sorted(ranking.items(),reverse=True, key=lambda item: item[1])}
 	return ranking
+
+
+def calculateQueryWeights(testvocab, inverted_index, testparagraphs, totaldocs):
+	"""
+	Calculate weight of the query passed - here query is a document 
+
+	Parameters:  
+	arg1: testvocab(set): Preprocessed list containing content of the documents  
+	arg2: inverted_index(dict): Inverted Index  
+	arg3: testparagraphs(list): paragraphs extracted from test document  
+	arg4: totaldocs(int): Number of documents in Training corpus  
+
+	Returns:  
+	weights(dictionary)  
+	&nbsp;&nbsp; Key: word in test document vocabulary  
+	&nbsp;&nbsp; Value: TfIdf score  
+	"""
+	weights = {}
+	
+	for word in testvocab:
+		count = 0
+		for para in testparagraphs:
+			for w in para:
+				if(w == word):
+					count += 1
+		weights[word] = count
+
+	for word in testvocab:
+		weights[word] = 1 + math.log(weights[word]+1)
+
+	
+	for word in testvocab:
+		if word not in inverted_index:
+			inverted_index[word] = []
+			tf_idf[word] = [0]*totaldocs
+		idfval = math.log((totaldocs+1)/(len(inverted_index[word])+1))
+		
+		weights[word] = round(weights[word] * idfval, 3)
+
+	return weights
+
+def calculateParaWeights(para, vocab, inverted_index, totaldocs):
+	"""
+	Calculate weight of the query passed - here query is a paragraph 
+
+	Parameters:  
+	arg1: para(list): List of tokens in the query paragraph  
+	arg2: testvocab(set): Preprocessed list containing content of the documents  
+	arg3: inverted_index(dict): Inverted Index  
+	arg4: totaldocs(int): Number of documents in Training corpus  
+
+	Returns:  
+	weights(numpy array) - contains weights of the words in the test para vocabulary for the passed para    
+	"""
+
+	weights = np.zeros(len(vocab))
+	for i,word in enumerate(vocab):
+		count = 0
+		for w in para:
+			if(w == word):
+				count += 1
+		weights[i] = count
+
+		for i in range(len(vocab)):
+			if(weights[i] != 0):
+				weights[i] = 1 + math.log(weights[i]+1)
+
+		for i,word in enumerate(vocab):
+			if word not in inverted_index:
+				inverted_index[word] = []
+				tf_idf[word] = [0]*totaldocs
+			idfval = math.log((totaldocs+1)/(len(inverted_index[word])+1))
+			weights[i] = round(weights[i] * idfval, 3)
+	return weights
+
+
+def rankDocsByCosineSimilarity(documents, testparagraphs, tf_idf, inverted_index):
+	ranking = {}
+	testvocab = set()
+	for para in testparagraphs:
+		for word in para:
+			testvocab.add(word)
+	testdoc = []
+	testdoc.append(testparagraphs)
+	query_tfidf = calculateQueryWeights(testvocab, inverted_index, testparagraphs, len(documents))
+	a = np.zeros(len(testvocab))
+	b = np.zeros(len(testvocab))
+
+	# print(query_tfidf)
+	
+	for j,doc in enumerate(documents):
+		i = 0
+		for word in testvocab:
+			if word in inverted_index and j in inverted_index[word]:
+				a[i] = tf_idf[word][j]
+			else:
+				a[i] = 0
+			b[i] = query_tfidf[word]
+			i += 1
+		ranking[j] = cosine_sim(a,b)
+
+	#sort ranking dict in reverse order by keys
+	ranking = {k: v for k, v in sorted(ranking.items(),reverse = True, key=lambda item: item[1])}
+	# print(ranking)
+	return ranking
+
+
+def cosine_sim(a,b):
+	"""
+	Calculate cosine of two vectors
+	
+	Parameters:  
+	arg1: a(list) - 1st vector
+	arg2: b(list) - 2nd vector
+
+	Return:
+	cos_sim(float) - Computed cosine
+	"""
+
+	cos_sim = 0
+	if np.linalg.norm(a)!=0 and np.linalg.norm(b)!=0:
+		cos_sim = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+	return cos_sim
+
+
 
 
 if __name__ == '__main__':
@@ -245,19 +370,46 @@ if __name__ == '__main__':
 		finaltokens = stemmer(stoplesstokens)
 		###
 		testparagraphs.append(finaltokens)
-
-
-
-	ranking = rankDocsBySimilarity(documents, testparagraphs, tf_idf)
+	
+	# print(len(testparagraphs), testparagraphs)
+	
 	
 
+	# ranking = rankDocsByMatchingSimilarity(documents, testparagraphs, tf_idf)
+	# Use when Matching Similarity is to be used to rank docs
+
+	ranking = rankDocsByCosineSimilarity(documents, testparagraphs, tf_idf, inverted_index)
 
 	#print top 10 matching documents
 	print("\nTop 10 documents matching the given test document in ranked order are: ")
-	print("Rank","	-	","Doc No", "	-	","Doc Name","		-	","Score")
+	print("Rank","	-	","Doc No", "	-	","Doc Name","		-	","Cosine Score")
 	cnt = 0
 	for i in ranking:
 		cnt += 1
 		print(cnt,"	-	",i, "		-	",filename[i],"	-	",ranking[i])
 		if(cnt>10):
 			break
+
+	#para as a query part
+	print("\n")
+	
+	totaldocs = len(documents)
+	totalmatches = 0
+	for k, tpara in enumerate(testparagraphs):
+		testparavocab = set()
+		for term in tpara:
+			testparavocab.add(term)
+		testparaweights = calculateParaWeights(tpara, testparavocab, inverted_index, totaldocs)
+		matchfound = 0
+		for i,doc in enumerate(documents):
+			for j,para in enumerate(doc):
+				paraweights = calculateParaWeights(para, testparavocab, inverted_index, totaldocs)
+				cos_sim = cosine_sim(testparaweights, paraweights)
+				if(cos_sim > 0.95):
+					matchfound = 1
+					totalmatches += 1
+					print("Paragraph ",k," from test document matches with paragraph ",j," from document ",i," - ",filename[i])
+					break
+			if(matchfound == 1):
+				break
+	print("Document uniqueness = ", round(100 - (totalmatches*100/len(testparagraphs)), 2),"%")
